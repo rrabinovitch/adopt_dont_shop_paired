fbe55b0a64d19de987a2fd6f38921e126e661a21
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-07 23:56:35 -0600

    Fix spacing

--- app/controllers/shelter_reviews_controller.rb
+++ app/controllers/shelter_reviews_controller.rb
@@ -3,9 +3,8 @@ class ShelterReviewsController < ApplicationController
   def new
     @shelter = Shelter.find(params[:id])
   end
-  
-  def create
 
+  def create
     shelter_review = ShelterReview.new({
               title: params[:title],
               rating: params[:rating],
@@ -17,22 +16,20 @@ class ShelterReviewsController < ApplicationController
     shelter_review.save
     redirect_to "/shelters/#{params[:id]}"
   end
-  
+
   def edit
     @shelter = Shelter.find(params[:shelter_id])
     @shelter_review = ShelterReview.find(params[:review_id])
   end
-  
+
   def update
     @shelter_review = ShelterReview.find(params[:review_id])
     @shelter_review.update(shelter_review_params)
     redirect_to "/shelters/#{params[:shelter_id]}"
   end
-  
+
   private
   def shelter_review_params
     params.permit(:title, :rating, :content, :picture)
   end
-  
-
 end
