a39cb5fccebb6db11106dc463e7fe77a9b6c5ba0
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-08 21:45:28 -0600

    Specfied in test to look for favorites indicator in the navbar

--- spec/features/pets/favorite_spec.rb
+++ spec/features/pets/favorite_spec.rb
@@ -6,8 +6,10 @@ RSpec.describe "Favorite indicator", type: :feature do
   it "In the navigation bar there is a link showing the number of pets in my favorites list" do
     
     visit "/"
+    favorites = 0
     
-    expect(page).to have_content("Favorites - 0")
-    
+    within("navbar") do
+      expect(page).to have_content("Favorites - #{favorites}")
+    end
   end
 end
