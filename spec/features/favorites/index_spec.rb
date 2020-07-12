0aedea2a98255f9c702524e9f2c85c990faa1956
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-12 10:50:34 -0600

    Complete user story 14

--- spec/features/favorites/index_spec.rb
+++ spec/features/favorites/index_spec.rb
@@ -7,40 +7,43 @@ RSpec.describe "Favorite's Index Page", type: :feature do
     pet1 = Pet.create(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: shelter.id, status: "Adoptable")
 
     visit "/pets/#{pet1.id}"
-    
+
     click_on 'Favorite'
-    
+
     within("navbar") do
       click_on "Favorites - 1"
     end
-    
+
     expect(current_path).to eq('/favorites')
     expect(page).to have_content(pet1.name)
     expect(page).to have_xpath("//img[@src='#{pet1.image}']")
   end
-  
+
   it "Gives the option to delete a pet in the favorites index page" do
     shelter = Shelter.create(name: "Primary Shelter", address: "123 Maple Ave.", city: "Denver", state: "CO", zip: "80438")
     pet1 = Pet.create(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: shelter.id, status: "Adoptable")
 
     visit "/pets/#{pet1.id}"
-    
+
     click_on 'Favorite'
-    
+
     within("navbar") do
       click_on "Favorites - 1"
     end
+
     expect(current_path).to eq("/favorites")
     expect(page).to have_button('Remove Pet From Favorites')
-    
+
     click_button 'Remove Pet From Favorites'
-    
+
     expect(current_path).to eq("/favorites")
     expect(page).to have_content("#{pet1.name} has been removed to your favorites list.")
     expect(page).to_not have_xpath("//img[@src='#{pet1.image}']")
+  end
 
-    
-    
+  it "No favorited pets" do
+    visit "/favorites"
+    expect(page).to have_content("No pets to see here. Explore pets here and select some of your favorites!")
   end
 end
 
@@ -64,7 +67,7 @@ RSpec.describe "Favorite indicator and index", type: :feature do
 
     expect(current_path).to eq('/favorites')
   end
-  
+
   it "The favorite indicator links to the favorites index page" do
     visit "/favorites"
 
@@ -74,5 +77,4 @@ RSpec.describe "Favorite indicator and index", type: :feature do
 
     expect(current_path).to eq('/favorites')
   end
-  
 end
