42515a9cd5155ff570dc038dca68adbc9aa164ed
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-13 10:44:04 -0600

    Fix duplicate variable name for display application details test

---
+++ app/views/adoption_applications/index_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe "Adoption applications index page", type: :feature do
+  it "text" do
+
+  end
+end
--- spec/features/adoption_applications/show_spec.rb
+++ spec/features/adoption_applications/show_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe "Adoption application show page", type: :feature do
     pet1 = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: shelter.id, status: "Adoptable")
     pet2 = Pet.create!(name: "George", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "11", sex: "Male", shelter_id: shelter.id, status: "Adoptable")
     adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
-    pet_adoption_application = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id)
-    pet_adoption_application = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id)
+    pet_adoption_application_1 = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id)
+    pet_adoption_application_2 = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id)
 
     visit "/adoption_applications/#{adoption_application.id}"
 
@@ -22,5 +22,15 @@ RSpec.describe "Adoption application show page", type: :feature do
     adoption_application.pets.each do |pet|
       expect(page).to have_content(pet.name)
     end
+  end
+
+  it "has link to view all applications for this pet" do
+    shelter = Shelter.create!(name: "Primary Shelter", address: "123 Maple Ave.", city: "Denver", state: "CO", zip: "80438")
+    pet1 = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: shelter.id, status: "Adoptable")
+    pet2 = Pet.create!(name: "George", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "11", sex: "Male", shelter_id: shelter.id, status: "Adoptable")
+    adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
+    pet_adoption_application = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id)
+    pet_adoption_application = PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id)
+
   end
 end
