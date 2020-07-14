8955a4ddf5008441cd37764e48c1e8e3553754c1
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-14 14:51:13 -0600

    Created test to notify if fields are missing when submitting shelter info for new shelters

--- spec/features/shelters/new_spec.rb
+++ spec/features/shelters/new_spec.rb
@@ -18,4 +18,22 @@ RSpec.describe "New shelter", type: :feature do
     expect(current_path).to eq("/shelters")
     expect(page).to have_content('Test Shelter')
   end
+  
+  it "It tells me if I missed an input field" do
+    visit "/shelters/new"
+    
+    fill_in 'Name', with: 'Test Shelter'
+    fill_in 'Address', with: ''
+    fill_in 'City', with: ''
+    fill_in 'State', with: ''
+    fill_in 'Zip', with: ''
+
+    click_on "Create Shelter"
+
+    expect(current_path).to eq("/shelters/new")
+
+    expect(page).to have_content('Unsuccessful shelter submission, please fill in the following fields prior to submission: ["address", "city", "state", "zip"]')
+
+    
+  end
 end
