417a1fbd13c26f482b3dfd8682942de98280806b
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-14 16:53:04 -0600

    Set up user story 25 test

--- spec/features/adoption_applications/approve_spec.rb
+++ spec/features/adoption_applications/approve_spec.rb
@@ -57,4 +57,25 @@ RSpec.describe "Application approval", type: :feature do
     visit "/pets/#{pet.id}/adoption_applications"
     expect(page).to have_link(adoption_application2.name)
   end
+
+  it "Approved applications can be revoked" do
+    shelter = Shelter.create!(name: "Primary Shelter", address: "123 Maple Ave.", city: "Denver", state: "CO", zip: "80438")
+    pet = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: shelter.id, status: "Adoptable")
+    adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
+    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet.id)
+
+    visit "/adoption_applications/#{adoption_application.id}"
+    click_on "Approve application for #{pet.name}"
+
+    visit "/adoption_applications/#{adoption_application.id}"
+    expect(page).to_not have_content("Approve application for #{pet.name}")
+    click_on "Revoke application for #{pet.name}"
+
+    expect(current_path).to eq("/adoption_applications/#{adoption_application.id}")
+    expect(page).to have_link("Approve application for #{pet.name}")
+
+    visit "/pets/#{pet.id}"
+    expect(page).to have_content("Adoption Status: Adoptable")
+    expect(page).to_not have_content("On hold")
+  end
 end
