7f0453f5f1e89a920f6d544b9949197e6379277f
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-15 16:51:42 -0600

    Add test to show I can delete shelters and their pets if there are no pending applications.

--- spec/features/shelters/destroy_spec.rb
+++ spec/features/shelters/destroy_spec.rb
@@ -79,4 +79,28 @@ RSpec.describe "Delete shelter", type: :feature do
   expect(page).to have_content("Unable to delete #{test_shelter.name}, the shelter has has pending applications")
 
   end
+  
+  it "I can delete a shelter and pets if it has no pending pet applications" do
+
+  test_shelter = Shelter.create!(name: "Test Shelter", address: "114 Test Address", city: "Test City", state: "CO", zip: "80202")
+  pet1 = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
+  pet2 = Pet.create!(name: "Buddy", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "3", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
+  adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
+  PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id, status: "Pending")
+  PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id, status: "Pending")
+  
+  visit "/shelters"
+  
+  expect(page).to have_content("#{test_shelter.name}")
+  expect(Shelter.all.empty?).to eq(false)
+  expect(Pet.all.empty?).to eq(false)
+  
+  click_link "Delete"
+  
+  expect(current_path).to eq("/shelters")
+  expect(page).to_not have_content("#{test_shelter.name}")
+  expect(Shelter.all.empty?).to eq(true)
+  expect(Pet.all.empty?).to eq(true)
+  
+  end
 end
