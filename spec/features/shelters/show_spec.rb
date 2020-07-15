aabbf97f49315257eb0ccdbdc379991a024e7c50
Ruthie Rabinovitch <rrabinovitch1@gmail.com>
2020-07-15 17:33:00 -0600

    Complete user stories 34 - 36

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "As a visitor", type: :feature do
     expect(page).to have_content("#{shelter_review.content}")
     expect(page).to have_xpath("//img[@src='#{shelter_review.picture}']")
   end
-  
+
   it "when I visit '/shelters/:id' I see available pet quantity, average review rating and number of applications on file" do
     test_shelter = Shelter.create!(name: "Test Shelter", address: "114 Test Address", city: "Test City", state: "CO", zip: "80202")
     shelter_review_1 = ShelterReview.create!(title: "Okay shelter!", rating: 2, content: "Too many sad dogs.", picture: "https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F20%2F2019%2F01%2Fpuppies.jpg", shelter_id: test_shelter.id)
@@ -38,14 +38,14 @@ RSpec.describe "As a visitor", type: :feature do
     pet1 = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
     pet2 = Pet.create!(name: "Buddy", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "3", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
     adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
-    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id)
-    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id)
+    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id, status: "Pending")
+    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id, status: "Pending")
 
     visit "/shelters/#{test_shelter.id}"
 
     expect(page).to have_content("Available Pets: 2")
     expect(page).to have_content("Average Review Rating: 3")
     expect(page).to have_content("Number of Applications on File: 2")
-    
+
   end
 end
--- spec/models/shelter_spec.rb
+++ spec/models/shelter_spec.rb
@@ -13,9 +13,9 @@ RSpec.describe Shelter, type: :model do
     pet1 = Pet.create!(name: "Bonnie", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "13", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
     pet2 = Pet.create!(name: "Buddy", image: "http://www.gsgsrescue.org/assets/files/dogs/2020/06/IMG_1639_1.jpg", approximate_age: "3", sex: "Female", shelter_id: test_shelter.id, status: "Adoptable")
     adoption_application = AdoptionApplication.create!(name: "Ruthie R", address: "1245 Turing Ave", city: "Denver", state: "CO", zip: "80250", phone_number: "253-555-1843", description: "I love animals and I want them all.")
-    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id)
-    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id)
-    
+    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet1.id, status: "Pending")
+    PetAdoptionApplication.create!(adoption_application_id: adoption_application.id, pet_id: pet2.id, status: "Pending")
+
     applications = test_shelter.applications_total
     expect(applications).to eq(PetAdoptionApplication.all.length)
   end
