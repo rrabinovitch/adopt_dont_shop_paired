22d0931f8e4a06a3b5772ebe32995005ae68e2ff
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-15 16:23:54 -0600

    changed delete_all to destroy

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,10 +1,17 @@
 class Shelter < ApplicationRecord
-  has_many :pets, dependent: :delete_all
+  has_many :pets, dependent: :destroy
   has_many :shelter_reviews, dependent: :delete_all
   
   def applications_total
     PetAdoptionApplication.joins(:pet).where('pets.shelter_id = ?', id).count
   end
+
+  def self.has_pending_applications?(shelter_id)
+    pending_applications = Pet.where(shelter_id: shelter_id, status:  'Pending')
+    if pending_applications.empty?
+      false
+    else
+      true
+    end
+  end
 end
-
-
