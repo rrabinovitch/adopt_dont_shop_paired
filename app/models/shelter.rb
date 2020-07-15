416a038c2daef4b8621243f613ef333b5aa93e61
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-14 23:22:01 -0600

    add applications_total method

--- app/models/shelter.rb
+++ app/models/shelter.rb
@@ -1,4 +1,10 @@
 class Shelter < ApplicationRecord
   has_many :pets, dependent: :delete_all
   has_many :shelter_reviews, dependent: :delete_all
+  
+  def applications_total
+    PetAdoptionApplication.joins(:pet).where('pets.shelter_id = ?', id).count
+  end
 end
+
+
