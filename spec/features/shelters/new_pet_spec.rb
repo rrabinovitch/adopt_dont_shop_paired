3219d46cda224d5d2e8511db017f8cef6a5bd55c
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-07-14 18:11:32 -0600

    Added flash notices for incomplete fields when creating pet to the create method

--- spec/features/shelters/new_pet_spec.rb
+++ spec/features/shelters/new_pet_spec.rb
@@ -25,10 +25,32 @@ RSpec.describe "As a visitor", type: :feature do
     fill_in 'Sex', with: 'Male'
     click_on 'Create Pet'
 
-    expect(current_path).to eq("/shelters/#{@shelter.id}/pets/")
+    expect(current_path).to eq("/shelters/#{@shelter.id}/pets")
     expect(page).to have_content('Happy')
     expect(page).to have_content('Happiest beagle ever!')
     expect(page).to have_content('8')
     expect(page).to have_content('Male')
+  end
+  
+  it "When creating a new pet it tells me if I missed an input field" do
+    @shelter = Shelter.create(name: "Shelter A")
+    
+    visit "/shelters/#{@shelter.id}/pets"
+    
+    click_link 'Create Pet'
+    
+    expect(current_path).to eq("/shelters/#{@shelter.id}/pets/new")
+    
+    fill_in 'Name', with: 'Happy'
+    fill_in 'Image', with: 'https://upload.wikimedia.org/wikipedia/commons/4/42/Beagle1.jpg'
+    fill_in 'Description', with: ''
+    fill_in 'Approximate age', with: ''
+    fill_in 'Sex', with: ''
+
+    click_on 'Create Pet'
+    save_and_open_page
+    expect(current_path).to eq("/shelters/#{@shelter.id}/pets/new")
+    expect(page).to have_content('Unsuccessful shelter submission, please fill in the following fields prior to submission: ["description", "approximate_age", "sex"]')
+
   end
 end
